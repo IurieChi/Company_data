e781c2a41758f3cc91ef3fbd2f5263ad1bd274a0
Iurie Chigai <124267735+IurieChi@users.noreply.github.com>
2024-03-28 17:40:57 +0100

    added default headers

---
+++ headers.py
@@ -0,0 +1,8 @@
+
+
+defoult_headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Refer': 'https://google.com',
+    'DNT': '1'
+}
